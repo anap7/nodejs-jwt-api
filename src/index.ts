03b6f04850f2aefe559590dadeef2073fba15454
anap7 <ana.flaviap7@hotmail.com>
2021-11-17 22:17:24 -0300

    feat: initial adjust

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.use(routes);
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log("🔥 Server Started Baby!");
+})
